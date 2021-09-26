824e6942f19de03d0618ce93a6bfd033d07712c3
Hmlessard <89013364+Hmlessard@users.noreply.github.com>
2021-09-26 16:26:25 -0500

    rest of questions and entering initials

--- assets/questions.js
+++ assets/questions.js
@@ -19,6 +19,36 @@ let questions = [{
         "Alerts",
         "Numbers",
     ]
+}, {
+    numb: 3,
+    question: "The condition in an if/else statement is enclosed with ______.",
+    answer: "Curly brackets",
+    options: [
+        "Quotes",
+        "Curly brackets",
+        "Parentheses",
+        "Square brackets",
+    ]
+}, {
+    numb: 4,
+    question: "Arrays in JavaScript can be used to store __________",
+    answer: "All of the above",
+    options: [
+        "Numbers and strings",
+        "Other arrays",
+        "Booleans",
+        "All of the above",
+    ]
+}, {
+    numb: 5,
+    question: "String values must be enclosed within ________ when being assigned to variables.",
+    answer: "quotes",
+    options: [
+        "Commas",
+        "Curly brackets",
+        "Quotes",
+        "Parentheses",
+    ]
 }
 
 ]
